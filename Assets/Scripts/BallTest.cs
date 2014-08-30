b08bb83f5e6cde0762d31d33e3de2eb4cb861720
Kaue Aftimus Rosa <kaue@kaueaftimus.com>
2014-08-30 14:31:44 -0700

    Adding project
    adding project

---
+++ Assets/Scripts/BallTest.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallTest : MonoBehaviour {
+	
+	void Start () {
+		this.rigidbody2D.AddForce (new Vector2(Random.value,Random.value)*500);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/LookAtTarget.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtTarget : MonoBehaviour {
+
+	public Transform targetToLookAt = null;
+
+	void Start() {
+		Screen.orientation = ScreenOrientation.Portrait;
+	}
+	
+	void Update () {
+		if(!targetToLookAt) return;
+		transform.LookAt (targetToLookAt);
+		transform.position = Vector3.Lerp(transform.position, new Vector3(Input.acceleration.x,Input.acceleration.y,transform.position.z),10);
+	
+	}
+
+	void OnGUI() {
+		GUI.Button (new Rect (0, 0, Screen.width * 0.2f, Screen.height * 0.2f), Input.acceleration.ToString ());
+
+	}
+}
