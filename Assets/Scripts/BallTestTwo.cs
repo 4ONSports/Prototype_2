4ce2bdeb3c11eadab17a4d542e7a3683e537906b
Kaue Aftimus Rosa <kaue.aftimus@gmail.com>
2014-09-01 13:03:41 -0700

    creating test 3

---
+++ Assets/Scripts/BallTestTwo.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallTestTwo : MonoBehaviour {
+
+	[SerializeField] float tapDistance = 0.6f;
+	[SerializeField] float force = 450f;
+
+	public void OnTap(Vector2 _tap) {
+		if (_tap.y < 0) {
+			if(Vector2.Distance(transform.position,_tap) <= tapDistance) {
+				this.rigidbody2D.velocity *= 0;
+				this.rigidbody2D.AddForce(new Vector2(transform.position.x-_tap.x,1) * force);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/InputManagerTwo.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManagerTwo : MonoBehaviour {
+
+	[SerializeField] private BallTestTwo ball = null;
+	private InputHandler ih = null;
+	
+	void Start() {
+		ih = this.GetComponent<InputHandler> ();
+	}
+	
+	void Update() {
+		if (InputHandler.isTap == true) {
+			ball.OnTap(Camera.main.ScreenToWorldPoint(new Vector3(InputHandler.swipe_startPos.x,InputHandler.swipe_startPos.y,-Camera.main.transform.position.z)));
+			return;
+		}
+	}
+}
